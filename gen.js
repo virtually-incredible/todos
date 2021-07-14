b97ea32db5c12cf7efd7938933fdd074ba5f9d51
yyk <yyk@mail.ru>
2021-07-14 16:27:38 -0400

    overdue task shown first, done tasks were remoded on summary

--- gen.js
+++ gen.js
@@ -5,7 +5,7 @@ gen.result_matrix = function(ex_map, settings) {
   m = [];
   executors = keys(ex_map).sort();
   executors.forEach(function(executor) {
-    var ongoings, overdues, dones, ex_h;
+    var ongoings, overdues, ex_h;
     ex_h = _.extend({}, {Value : executor}, settings['executor']);
     ongoings = ex_map[executor]['ongoing'].map(function(x) {
       return _.extend({}, {Value : x}, settings['ongoing']);
@@ -13,13 +13,9 @@ gen.result_matrix = function(ex_map, settings) {
     overdues = ex_map[executor]['overdue'].map(function(x) {
       return _.extend({}, {Value : x}, settings['ovedue']);
     });
-    dones = ex_map[executor]['done'].map(function(x) {
-      return _.extend({}, {Value : x}, settings['done']);
-    });
     m.push([ex_h]
-      .concat(ongoings)
       .concat(overdues)
-      .concat(dones)
+      .concat(ongoings)
     );
   });
   maximum = max(m.map(function(r) {return r.length;}));
